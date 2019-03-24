9844c32716e07027b84c247a6105bddd11f47398
Alex Bradley <alex.robert.bradley@gmail.com>
2019-03-24 11:16:59 -0400

    added seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Event.destroy_all
+Attendee.destroy_all
 
 user = User.create(email: "test@user.com",
                     first_name: "test",
@@ -15,6 +16,19 @@ user = User.create(email: "test@user.com",
                     password: "abc123",
                     password_confirmation: "abc123")
 
+user2 = User.create(email: "test2@user.com",
+                    first_name: "test2",
+                    last_name: "user",
+                    username: "testuser2",
+                    password: "abc123",
+                    password_confirmation: "abc123")
+
+user3 = User.create(email: "test3@user.com",
+                    first_name: "test3",
+                    last_name: "user",
+                    username: "testuser3",
+                    password: "abc123",
+                    password_confirmation: "abc123")
 Event.create([
     { title: "Test 1 Event",
       user: user,
@@ -32,4 +46,18 @@ Event.create([
       user: user,
       description: "Test Desc",
     },
+    { title: "Test 5 Event",
+      user: user2,
+      description: "Test Desc",
+    },
+    { title: "Test 6 Event",
+      user: user2,
+      description: "Test Desc",
+    },
   ])
+
+Attendee.create(user: user2, event: Event.first)
+Attendee.create(user: user3, event: Event.first)
+Attendee.create(user: user, event: Event.last)
+Attendee.create(user: user3, event: Event.last)
+Attendee.create(user: user2, event: Event.first)
